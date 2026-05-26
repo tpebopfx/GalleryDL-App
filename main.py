8d13c46d08b197f06c35f7fcaebdc7e2538999c6
tpebopfx <dr.eliasalabd@gmail.com>
2026-05-27 00:06:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import app
+
+# Запрашиваем разрешения для Android ПЕРЕД запуском сервера
+try:
+    from android.permissions import request_permissions, Permission
+    request_permissions([
+        Permission.INTERNET, 
+        Permission.WRITE_EXTERNAL_STORAGE, 
+        Permission.READ_EXTERNAL_STORAGE
+    ])
+except ImportError:
+    print("Запуск не на Android, пропускаем запрос разрешений.")
+
 if __name__ == '__main__':
     # Запускаем наш Flask-сервер
     app.app.run(host='127.0.0.1', port=5000)
+    
